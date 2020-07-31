8d96c7f331dace7ffdebda7b30916ad53d24197f
jacob kleiman <jacobakleiman@gmail.com>
2020-07-30 22:23:11 -0700

    adds author model

---
+++ models/author.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const Article = require('./article')
+
+const authorSchema = new mongoose.Schema({
+    name: String,
+    // creating a relationship through a reference
+    articles: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article'
+    }]
+})
+
+const Author = mongoose.model('Author', authorSchema)
+
+module.exports = Author
